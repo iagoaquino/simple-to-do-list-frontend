d55b73f16e369b634fef5be723156d21d38c646f
iago <iagodeaquino10@gmail.com>
2025-09-03 15:02:29 -0300

    Header subcomponent created

---
+++ src/sub_components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<div style={{ height: 'auto' }} className="header">
+			<h1 className="no-margin">Minha lista de afazeres</h1>
+		</div>
+	);
+};
+
+export default Header;
